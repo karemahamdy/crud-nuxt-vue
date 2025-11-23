3063bd4d0e30f3fccdf3ae2704ca5632247bed27
karemahamdy20 <K.Hamdy@tera.ltd>
2025-11-23 09:38:10 +0200

    ..

--- app/services/items.js
+++ app/services/items.js
@@ -3,4 +3,4 @@ import { api } from "~/utils/api";
 export const getItems = () => api.get("/items");
 export const addItem   = (payload) => api.post("/items", payload);
 export const updateItem = (id, payload) => api.put(`/items/${id}`, payload);
-export const deleteItem = (id) => api.delete(`/items/${id}`)
+export const deleteItem = (id) => api.delete(`/items/${id}`);
