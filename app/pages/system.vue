9ae32b6d9856c680b566c57742118c7302a28017
karemahamdy <karemahamdy51@gmail.com>
2025-11-29 11:55:51 +0200

    create :  design system of color

---
+++ app/pages/system.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="flex flex-col justify-between gap-4 ">
+    <div class="flex gap-2">
+<button class="bg-primary-50 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-100 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-200 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-300 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-400 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-500 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-600 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-700 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-800 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-900 text-white px-4 py-2 rounded">primary</button>
+<button class="bg-primary-950 text-white px-4 py-2 rounded">danger</button>
+</div>
+
+<div class="flex gap-2">
+<button class="bg-grey-50 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-100 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-200 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-300 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-400 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-500 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-600 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-700 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-800 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-900 text-white px-4 py-2 rounded">grey</button>
+<button class="bg-grey-950 text-white px-4 py-2 rounded">danger</button>
+</div>
+
+<div class="flex gap-2">
+<button class="bg-success-50 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-100 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-200 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-300 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-400 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-500 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-600 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-700 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-800 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-900 text-white px-4 py-2 rounded">success</button>
+<button class="bg-success-950 text-white px-4 py-2 rounded">success</button>
+</div>
+
+<div class="flex gap-2">
+<button class="bg-warning-50 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-100 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-200 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-300 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-400 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-500 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-600 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-700 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-800 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-900 text-white px-4 py-2 rounded">warning</button>
+<button class="bg-warning-950 text-white px-4 py-2 rounded">danger</button>
+</div>
+
+<div class="flex gap-2">
+    <button class="bg-danger-50 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-100 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-200 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-300 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-400 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-500 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-600 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-700 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-800 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-900 text-white px-4 py-2 rounded">danger</button>
+    <button class="bg-danger-950 text-white px-4 py-2 rounded">danger</button>
+    </div>
+
+    </div>
+</template>
